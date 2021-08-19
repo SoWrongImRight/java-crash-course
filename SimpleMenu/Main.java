98b7edb3b053d57f595351f52e5f8cc2a7a6a4d4
Russ Carroll <info@rcarroll.dev>
2021-08-18 22:55:08 -0400

    Initial i/o lesson project.

---
+++ SimpleMenu/Main.java
@@ -0,0 +1,20 @@
+package academy.learnprogramming;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scanner = new_Scanner(System.in);
+
+    System.out.println("Please choose one of the following options");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("");
+
+    scanner.close();
+  }
+}
